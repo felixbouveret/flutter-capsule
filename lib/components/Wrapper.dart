83eb6b64b9f13030eca0ad8ba34be29764a9fc6c
Félix Bouveret <bouveret.felix@gmail.com>
2021-03-21 20:19:38 +0100

    Add Wrapper

--- lib/components/Wrapper.dart
+++ lib/components/Wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 
 class Wrapper extends StatelessWidget {
-  const Wrapper({Key key}) : super(key: key);
+  const Wrapper({Key key, this.child}) : super(key: key);
+
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: child,
+    );
   }
 }
