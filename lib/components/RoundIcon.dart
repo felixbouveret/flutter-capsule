b4381f55e856c5a3ed78039cba6610b9f0ce0f41
Félix Bouveret <bouveret.felix@gmail.com>
2021-03-21 21:27:34 +0100

    Add RoundIcon

---
+++ lib/components/RoundIcon.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+
+class RoundIcon extends StatelessWidget {
+  const RoundIcon({Key key, this.image, this.size, this.backgroundColor})
+      : super(key: key);
+
+  final Widget image;
+  final double size;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(size),
+        color: backgroundColor,
+      ),
+      child: image,
+    );
+  }
+}
