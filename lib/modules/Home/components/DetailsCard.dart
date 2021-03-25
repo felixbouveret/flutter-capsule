e53152c274e354162c9ac63eab96a74c6f0ffc69
Félix Bouveret <bouveret.felix@gmail.com>
2021-03-25 09:52:26 +0100

    Add DetailsCard with custom images

--- lib/modules/Home/components/DetailsCard.dart
+++ lib/modules/Home/components/DetailsCard.dart
@@ -1,19 +1,58 @@
+import 'package:capsule/utils/custom_icons_icons.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class DetailsCard extends StatelessWidget {
   const DetailsCard(
-      {Key key, this.backgroundColor, this.image, this.label, this.remove})
+      {Key key, this.colors, this.image, this.label, this.id, this.remove})
       : super(key: key);
 
-  final Object backgroundColor;
-  final Object image;
+  final Object colors;
+  final Widget image;
   final String label;
+  final String id;
   final Function remove;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('e'),
+      width: 160,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          gradient: LinearGradient(
+            colors: colors,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  padding: EdgeInsets.only(left: 20, top: 24), child: image),
+              InkWell(
+                onTap: () {
+                  remove(id);
+                },
+                child: Container(
+                    padding: EdgeInsets.only(right: 16, top: 16),
+                    child: Icon(CustomIcons.cross)),
+              )
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 20, right: 20, bottom: 24),
+            child: Text(
+              label,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
