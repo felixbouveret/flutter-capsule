45a4934bc930c08560c3c3850b61bc4e1e247b8c
Félix Bouveret <bouveret.felix@gmail.com>
2021-03-27 00:31:49 +0100

    Add modal

---
+++ lib/utils/showModal.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'custom_icons_icons.dart';
+
+Future showModal(context, child, title) {
+  return showModalBottomSheet(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+      ),
+      backgroundColor: Color(0xff19181E),
+      context: context,
+      elevation: double.infinity,
+      useRootNavigator: true,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Container(
+          height: double.infinity,
+          padding: EdgeInsets.only(top: 64, left: 24, right: 24),
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 40),
+                child: Stack(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        CustomIcons.arrowright,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Center(
+                        child: Text(
+                      title,
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ],
+                ),
+              ),
+              child
+            ],
+          ),
+        );
+      });
+}
